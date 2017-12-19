46f92dab4651b3208822f7aceb604e2fc5702f83
Simon Cropp <simon.cropp@gmail.com>
2017-12-19 22:02:59 +1100

    refs

--- Tests/WeaverTests.cs
+++ Tests/WeaverTests.cs
@@ -1,55 +1,24 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
 using Fody;
-using Mono.Cecil;
 using Xunit;
 #pragma warning disable 618
 
-public class WeaverTests:IDisposable
+public class WeaverTests
 {
-    Assembly assembly;
-    string newAssemblyPath;
-    string assemblyPath;
+    TestResult testResult;
 
     public WeaverTests()
     {
-        assemblyPath = Path.Combine(CodeBaseLocation.CurrentDirectory, "AssemblyToProcess.dll");
-
-        newAssemblyPath = assemblyPath.Replace(".dll", "2.dll");
-        File.Copy(assemblyPath, newAssemblyPath, true);
-
-        using (var moduleDefinition = ModuleDefinition.ReadModule(assemblyPath))
-        {
-            var weavingTask = new ModuleWeaver
-            {
-                ModuleDefinition = moduleDefinition
-            };
-
-            weavingTask.Execute();
-            moduleDefinition.Write(newAssemblyPath);
-        }
-
-        assembly = Assembly.Load(File.ReadAllBytes(newAssemblyPath));
+        var weavingTask = new ModuleWeaver();
+        testResult = weavingTask.ExecuteTestRun("AssemblyToProcess.dll");
     }
 
     [Fact]
     public void ValidateHelloWorldIsInjected()
     {
-        var type = assembly.GetType("TheNamespace.Hello");
+        var type = testResult.Assembly.GetType("TheNamespace.Hello");
         var instance = (dynamic)Activator.CreateInstance(type);
 
         Assert.Equal("Hello World", instance.World());
-    }
-
-    [Fact]
-    public void PeVerify()
-    {
-        PeVerifier.ThrowIfDifferent(assemblyPath, newAssemblyPath, new []{ "0x80070002" });
-    }
-
-    public void Dispose()
-    {
-       File.Delete(newAssemblyPath);
     }
 }
